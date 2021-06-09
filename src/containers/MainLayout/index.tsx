01d70388d5138a110e01f6b3d42b8300c1ceaf24
halit <halitfuatbatur@gmail.com>
2021-06-09 10:22:28 +0200

    add routing in main layout

--- src/containers/MainLayout/index.tsx
+++ src/containers/MainLayout/index.tsx
@@ -5,13 +5,25 @@ import { Layout, Menu } from "antd";
 
 const { Header, Content, Footer } = Layout;
 
+const componentsRenderer: Record<string, JSX.Element> = {
+  "1": <TableView />,
+  "2": <GraphView />,
+  "3": <div></div>,
+};
+
 const MainLayout = () => {
+  const [selectedMenuItem, setSelectedMenuItem] = React.useState("1");
   return (
     <div>
       <Layout className="layout">
         <Header>
           <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            selectedKeys={[selectedMenuItem]}
+            onSelect={(e) => setSelectedMenuItem(e.key)}
+          >
             <Menu.Item key="1">Table View</Menu.Item>
             <Menu.Item key="2">Graph View</Menu.Item>
             <Menu.Item key="3">About</Menu.Item>
@@ -19,8 +31,7 @@ const MainLayout = () => {
         </Header>
         <Content style={{ padding: "0 50px" }}>
           <div className="site-layout-content">
-            <TableView />
-            <GraphView />
+            {componentsRenderer[selectedMenuItem]}
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>Created by Halit Batur</Footer>
